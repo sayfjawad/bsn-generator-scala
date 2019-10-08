e68e84dd0ea18a04e344ea6e6ba1f50b783d8867
sayf4all <sayf4all@hotmail.com>
2019-10-08 12:53:24 +0200

    Update BsnService.scala

--- src/main/scala/nl/multicode/bsn/service/BsnService.scala
+++ src/main/scala/nl/multicode/bsn/service/BsnService.scala
@@ -1,5 +1,25 @@
 package nl.multicode.bsn.service
 
-class BsnService {
+import nl.multicode.bsn.validate.BsnElfproef
 
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+class BsnService(bsnElfproef: BsnElfproef) {
+  private val MAX = 999999999
+  private val MIN = 100000000
+
+
+  def generateRandomBsnNummers(): String = {
+    while (true) {
+      val randomNumber = ThreadLocalRandom.current.nextInt(MIN, MAX)
+      if (isValidBsn(randomNumber.toString)) {
+        return randomNumber.toString
+      }
+    }
+    null
+  }
+
+  def isValidBsn(bsn: String): Boolean = {
+    bsnElfproef.isElfproef(bsn)
+  }
 }
